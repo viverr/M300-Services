de6b3548cf5faef1f5c18a11ce03505909bef225
viverr <vincent.parlavecchio.holgado@gmail.com>
2022-03-24 08:17:10 +0100

    Vagrant_Aktualisierung

--- lb02/db.sh
+++ lb02/db.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# ROOT SETZUNG - DER BENUTZER WIRD ERSTELLT ALS ROOT
+# ROOT SETZUNG - DER ADMIN BENUTZER WIRD ERSTELLT ALS ROOT
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
+#-----------------------
 
-# INSTALL MYSQL
+#-----INSTALL MYSQL-----
 sudo apt-get install -y mysql-server
-
+#-----------------------
 
 #-----CONFIG STAGE------
 # PORT ÖFFNEN
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+#-----------------------
 
 # ACTIVATE REMOTE ACCESS
 mysql -uroot -padmin <<%EOF%
@@ -23,8 +25,9 @@ mysql -uroot -padmin <<%EOF%
 	INSERT INTO DB-LB02 VALUE ("TESTWERT","Dieser Wert sollte angezeigt werden.");
 	quit
 %EOF%
-
+#-----------------------
 
 #-----RESTART-----
 # MYSQL NEUSTARTEN - AKTUALISIERUNG
 sudo service mysql restart
+#-----------------------
